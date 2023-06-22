e046ed4162cb517e3ff1db5f7c260f9f807ac387
Ned Pankhurst <ned.pankhurst@gmail.com>
2023-06-23 07:14:46 +1000

    Finished auth and db models and query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { NavBar } from "@/components/NavBar"
+import Providers from "@/components/Providers"
+import { Toaster } from "@/components/ui/Toaster"
 import { cn } from "@/lib/utils"
 import "@/styles/globals.css"
 import { Inter } from "next/font/google"
@@ -12,8 +14,10 @@ const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({
   children,
+  authModal,
 }: {
   children: React.ReactNode
+  authModal: React.ReactNode
 }) {
   return (
     <html
@@ -24,10 +28,18 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-        <NavBar />
-        <div className="container max-w-7xl mx-auto h-full pt-12">
-          {children}
-        </div>
+        <Providers>
+          {/* @ts-expect-error server component */}
+          <NavBar />
+
+          {authModal}
+
+          <div className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </div>
+
+          <Toaster />
+        </Providers>
       </body>
     </html>
   )
