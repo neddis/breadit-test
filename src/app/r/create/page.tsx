0d1f58d72a84a7f0b12cee328f7047bd4b9e58be
Ned <ned.pankhurst@gmail.com>
2023-06-23 16:27:38 +1000

    Doing add post

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -43,6 +43,15 @@ const Page = () => {
           return loginToast()
         }
       }
+
+      toast({
+        title: "There was an error.",
+        description: "Could not create subreddit.",
+        variant: "destructive",
+      })
+    },
+    onSuccess: (data) => {
+      router.push(`/r/${data}`)
     },
   })
 
