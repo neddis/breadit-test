5ace634f1c7d5da7aa4264dceb66a8d9d59075c8
Ned Pankhurst <ned.pankhurst@gmail.com>
2023-06-27 13:53:53 +1000

    Fixed another error

--- src/components/EditorOutput.tsx
+++ src/components/EditorOutput.tsx
@@ -4,9 +4,7 @@ import dynamic from "next/dynamic"
 import Image from "next/image"
 import { FC } from "react"
 
-const Output = dynamic(
-  async () => (await import("editorjs-react-renderer")).default
-)
+const Output = dynamic(() => import("editorjs-react-renderer"), { ssr: false })
 
 interface EditorOutputProps {
   content: any
